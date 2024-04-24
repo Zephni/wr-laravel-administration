8bdaee865aeefe5bdc277a81569c4df465675d8e
Craig Dennis <craig@deliverysoftware.co.uk>
2024-04-24 13:01:08 +0100

    Refactor FormComponent and Input class, add Text class for generating input fields

--- src/Classes/FormComponents/FormComponent.php
+++ src/Classes/FormComponents/FormComponent.php
@@ -14,6 +14,32 @@ class FormComponent
     public $attributes;
 
     /**
+     * FormComponent constructor.
+     *
+     * @param string $name
+     * @param mixed $value
+     */
+    public function __construct(string $name, mixed $value)
+    {
+        $this->attributes = [
+            'name' => $name,
+            'value' => $value,
+        ];
+    }
+
+    /**
+     * Make method (can be used in any class that extends FormComponent).
+     *
+     * @param string $name
+     * @param mixed $value
+     * @return static
+     */
+    public static function make(string $name, mixed $value): static
+    {
+        return new static($name, $value);
+    }
+
+    /**
      * Return view component.
      *
      * @param mixed $inject
--- src/Classes/FormComponents/Text.php
+++ src/Classes/FormComponents/Text.php
@@ -5,31 +5,15 @@ namespace WebRegulate\LaravelAdministration\Classes\FormComponents;
 use Illuminate\View\View;
 use Illuminate\Support\Str;
 use WebRegulate\LaravelAdministration\Classes\WRLAHelper;
+use WebRegulate\LaravelAdministration\Classes\ManageableModel;
 
 /**
  * Class Input
  *
  * This class is responsible for generating input fields.
  */
-class Input extends FormComponent
+class Text extends FormComponent
 {
-    /**
-     * Input constructor.
-     *
-     * @param string $name
-     * @param string $label
-     * @param string $value
-     */
-    public function __construct(
-        public string $name = '',
-        public string $value = '',
-    ) {
-        $this->attributes = [
-            'name' => $name,
-            'value' => $value,
-        ];
-    }
-
     /**
      * Render the input field.
      *
