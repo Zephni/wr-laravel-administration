f46d26bf08362c626b8e1abe9a7a3d8f031e06f1
Craig Dennis <craig@deliverysoftware.co.uk>
2024-04-28 13:52:36 +0100

    Add Textarea form component and corresponding view. Added 3 json fields to wrla user

---
+++ src/Classes/FormComponents/Textarea.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace WebRegulate\LaravelAdministration\Classes\FormComponents;
+
+use WebRegulate\LaravelAdministration\Enums\PageType;
+use WebRegulate\LaravelAdministration\Classes\WRLAHelper;
+
+/**
+ * Class Input
+ *
+ * This class is responsible for generating input fields.
+ */
+class Textarea extends FormComponent
+{
+    /**
+     * Render the input field.
+     *
+     * @param PageType $upsertType
+     * @return mixed
+     */
+    public function render(PageType $upsertType): mixed
+    {
+        return view(WRLAHelper::getViewPath('components.forms.textarea'), [
+            'name' => $this->attributes['name'],
+            'label' => str(str_replace('_', ' ', $this->attributes['name']))->title(),
+            'value' => $this->attributes['value'],
+            'attr' => collect($this->attributes)
+                ->forget(['name', 'value'])
+                ->toArray(),
+        ])->render();
+    }
+}
---
+++ src/resources/views/components/forms/textarea.blade.php
@@ -0,0 +1,24 @@
+@props(['attr' => [], 'label' => null, 'id' => '', 'name', 'value' => '', 'required' => false, 'autofocus' => false, 'readonly' => false])
+
+@php
+    // Set id from name if unset
+    $id = empty($id) ? 'wrinput-'.$name : $id;
+@endphp
+
+@if(!empty($label))
+    {!! view($WRLAHelper::getViewPath('components.forms.label'), [
+        'id' => $id,
+        'label' => $label
+    ])->render() !!}
+@endif
+
+<textarea {{ $attributes->merge([
+    'id' => $id,
+    'name' => $name,
+    'class' => 'block w-full mt-2 px-3 py-1 border border-slate-400 dark:border-slate-600 bg-slate-200 dark:bg-slate-900
+        focus:outline-none focus:ring-1 focus:ring-primary-500 dark:focus:ring-primary-500 rounded-md shadow-sm placeholder-slate-400 dark:placeholder-slate-600'
+])->merge($attr) }}>{{ old($name, $value) }}</textarea>
+
+@error($name)
+    <p class="text-sm text-red-500 mt-2">{{ $message }}</p>
+@enderror
