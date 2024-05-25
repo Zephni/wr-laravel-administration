8b9888f73741d9cdbaf62e347d3660be5612c841
Craig Dennis <craig@deliverysoftware.co.uk>
2024-05-25 12:25:41 +0100

    chore: Refactor debug information in browse page

--- src/resources/views/themes/default/livewire/manageable-models/browse.blade.php
+++ src/resources/views/themes/default/livewire/manageable-models/browse.blade.php
@@ -106,9 +106,11 @@
     @endif
 
     @if($user->getSetting('debug') == true)
-        <p class="border border-slate-300 rounded-md p-2 mt-10">
+        <div class="border border-slate-300 rounded-md p-2 mt-10 text-slate-500">
+            <p class=" text-sm font-semibold">Debug Information:</p>
+            <hr class="my-1 border-slate-300">
             {{ $debugMessage }}
-        </p>
+        </div>
     @endif
 
 </div>
