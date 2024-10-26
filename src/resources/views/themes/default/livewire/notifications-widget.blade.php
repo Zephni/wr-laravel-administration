1c385e45aa8561a69e4cad96564271eecb8c723d
Craig Dennis <craig@deliverysoftware.co.uk>
2024-10-26 12:21:31 +0100

    Styling fixes for notification widget

--- src/resources/views/themes/default/livewire/notifications-widget.blade.php
+++ src/resources/views/themes/default/livewire/notifications-widget.blade.php
@@ -22,15 +22,15 @@
         </div>
     </div>
 
-    <div class="rounded-lg shadow-lg shadow-slate-300 dark:shadow-slate-850">
+    <div class="w-full max-w-full rounded-lg shadow-lg shadow-slate-300 dark:shadow-slate-850">
         <div class="flex flex-col text-sm bg-slate-100 dark:bg-slate-700 text-slate-800 dark:text-slate-300 rounded-lg overflow-hidden">
             <!-- Header Row -->
-            <table class="w-full table-auto text-left">
+            <table class="w-full table-auto text-left align-middle">
                 <thead>
                     <tr class="border-b bg-slate-700 dark:bg-slate-400 text-slate-100 dark:text-slate-800 border-slate-400 dark:border-slate-600 py-2">
                         <th class="px-2 py-2 font-medium">Type</th>
                         <th class="px-2 py-2 font-medium">Message</th>
-                        <th class="px-2 py-2 font-medium">Date</th>
+                        <th class="px-2 py-2 font-medium">Created</th>
                         <th class="px-2 py-2 font-medium"></th>
                     </tr>
                 </thead>
@@ -41,24 +41,26 @@
                             $definition = $notification->getDefinition();
                         @endphp
                         <tr target="_blank" class="bg-slate-100 dark:bg-slate-700 odd:bg-slate-200 dark:odd:bg-slate-800 py-1">
-                            <td style="font-size: 13px; line-height: 13px;" class="px-2 py-2 truncate font-medium [&_a]:underline">{!! $definition->getTitle() !!}</td>
+                            <td style="font-size: 13px;" class="px-2 py-2 truncate font-medium [&_a]:underline">{!! $definition->getTitle() !!}</td>
                             {{-- String replace <a href=" with <a target="_blank" href=" --}}
-                            <td style="font-size: 13px; line-height: 13px;" class="px-2 py-2 truncate [&_a]:font-medium [&_a]:underline">{!! str_replace('<a href=', '<a target="_blank" href=', $definition->getMessage()) !!}</div>
-                            <td style="font-size: 13px; line-height: 13px;" class="px-2 py-2 truncate">{{ $notification->created_at->format('d/m/Y') }}</td>
-                            <td style="" class="px-2 py-2 gap-3 truncate flex justify-end items-center">
-                                {{-- Notification buttons --}}
-                                @foreach($notification->getFinalButtons() as $button)
-                                    {!! $button !!}
-                                @endforeach
-    
-                                @if($notification->read_at !== null)
-                                    <i class="fas fa-check-circle text-primary-500 text-lg mr-1" title="Read / Completed"></i>
-                                @endif
+                            <td style="font-size: 13px;" class="text-wrap px-2 py-2 truncate [&_a]:font-medium [&_a]:underline">{!! str_replace('<a href=', '<a target="_blank" href=', $definition->getMessage()) !!}</div>
+                            <td style="font-size: 13px;" class="px-2 py-2 truncate">{{ $notification->created_at->format('d/m/Y H:i') }}</td>
+                            <td class="px-2 py-2">
+                                <div class="flex justify-end items-center gap-3 truncate">
+                                    {{-- Notification buttons --}}
+                                    @foreach($notification->getFinalButtons() as $button)
+                                        {!! $button !!}
+                                    @endforeach
+        
+                                    @if($notification->read_at !== null)
+                                        <i class="fas fa-check-circle text-primary-500 text-lg mr-1" title="Read / Completed"></i>
+                                    @endif
+                                </div>
                             </td>
                         </tr>
                     @empty
                         <tr class="!bg-slate-100 dark:bg-slate-700 odd:bg-slate-200 dark:odd:bg-slate-800 py-3">
-                            <td colspan="4" class="px-2 py-8 text-slate-500 dark:text-slate-300 text-center text-base">
+                            <td colspan="4" class="px-2 py-6 text-slate-500 dark:text-slate-300 text-center text-base">
                                 @if($statusFilter === 'unread')
                                     <i class="fas fa-check-circle text-primary-500 mr-1"></i>
                                     All tasks complete!
