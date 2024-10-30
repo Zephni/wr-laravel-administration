beb3c532db881f860fad3f230159adec5ef88ebb
Craig Dennis <craig@deliverysoftware.co.uk>
2024-10-30 18:37:45 +0000

    Minor style change for consistancy

--- src/resources/views/themes/default/dashboard.blade.php
+++ src/resources/views/themes/default/dashboard.blade.php
@@ -6,10 +6,10 @@
     {{-- Styled dashboard title --}}
     <div class="pb-6">
         <h1 class="text-2xl font-light">
-            <i class="fas fa-tachometer-alt text-slate-500 dark:text-white mr-1"></i>
+            <i class="fas fa-tachometer-alt text-slate-700 dark:text-white mr-1"></i>
             Dashboard
         </h1>
-        <hr class="border-b border-slate-300 w-80 mt-1 mb-3">
+        <hr class="border-b border-slate-400 w-80 mt-1 mb-3">
     </div>
 
     {{-- Notifications widget --}}
