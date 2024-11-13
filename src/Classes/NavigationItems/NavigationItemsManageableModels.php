7d46617c1d28e1f69f64af59c2f7a53ee763e2fd
Craig Dennis <craig@deliverysoftware.co.uk>
2024-11-13 12:37:47 +0000

    Refactor navigation item handling by replacing NavigationItemsAllManageableModels with NavigationItemsManageableModels for improved flexibility and model-specific imports

---
+++ src/Classes/NavigationItems/NavigationItemsManageableModels.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace WebRegulate\LaravelAdministration\Classes\NavigationItems;
+
+use WebRegulate\LaravelAdministration\Classes\ManageableModel;
+use WebRegulate\LaravelAdministration\Classes\WRLAHelper;
+
+class NavigationItemsManageableModels extends NavigationItem
+{
+    /**
+     * Import all manageable models as array of nav items
+     *
+     * @return array
+     */
+    public static function import(array $manageableModelClasses = []): array
+    {
+        return self::getAManageableModelsNavigationItems($manageableModelClasses);
+    }
+
+    /**
+     * Retrieves an array of navigation items for all manageable models.
+     *
+     * @return array An array of NavigationItemManageableModel objects representing the navigation items.
+     */
+    public static function getAManageableModelsNavigationItems(array $manageableModelClasses = []): array
+    {
+        $navItems = [];
+
+        if(empty(ManageableModel::$manageableModels)) {
+            return $navItems;
+        }
+
+        // If passed manageable model classes array is empty, get all manageable model classes
+        if(empty($manageableModelClasses)) {
+            $manageableModelClasses = array_keys(WRLAHelper::$globalManageableModelData);
+        }
+
+        foreach ($manageableModelClasses as $manageableModelClass) {
+            $nimm = $manageableModelClass::getNavigationItem();
+            
+            if(WRLAHelper::$globalManageableModelData[ltrim($manageableModelClass, '\\')]['hideFromNavigation']) {
+                continue;
+            }
+
+            $navItems[] = $nimm;
+        }
+
+        return $navItems;
+    }
+}
