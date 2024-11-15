3ac041df5d12909d2158a0359a80bf9d81e138aa
Craig Dennis <craig@deliverysoftware.co.uk>
2024-11-15 10:47:58 +0000

    Import data modal progress

--- src/Livewire/ImportDataModal.php
+++ src/Livewire/ImportDataModal.php
@@ -6,6 +6,7 @@ use Livewire\Attributes\Rule;
 use Livewire\WithFileUploads;
 use Livewire\Attributes\Validate;
 use LivewireUI\Modal\ModalComponent;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\Storage;
 use WebRegulate\LaravelAdministration\Classes\WRLAHelper;
 use WebRegulate\LaravelAdministration\Classes\ManageableModel;
@@ -16,19 +17,61 @@ class ImportDataModal extends ModalComponent
 
     public $manageableModelClass;
 
-    #[Rule(['required', 'mimes:csv'])]
-    public $file;
+    #[Rule(['required', 'file', 'mimes:csv', 'max:5120'])]
+    public $file = null;
 
+    public array $data = [
+        'headers' => [],
+        'rows' => [],
+        'tableColumns' => [],
+        'headersMappedToColumns' => [],
+    ];
+
+    public array $debugInfo;
+
+    /**
+     * Updated file hook, runs after attribute validation
+     * 
+     * @return void
+     */
     public function updatedFile()
     {
+        // If any errors
         if (!$this->getErrorBag()->isEmpty()) {
             // Delete the temporary file if validation fails
             if ($this->file) {
-                // Delete the temporary file from the Storage livewire-tmp folder
                 Storage::disk('local')->delete('livewire-tmp/' . $this->file->getFilename());
             }
-            $this->file = null; // Clear the file property
+
+            // Clear the file property
+            $this->file = null;
+            return;
         }
+
+        // If valid, we now need to check the CSV file is valid, if not, manually add an error
+        $filePath = $this->file->getRealPath();
+        $fileData = array_map('str_getcsv', file($filePath));
+
+        // Set first row as headers
+        $this->data['headers'] = array_shift($fileData);
+
+        // Set row data
+        $this->data['rows'] = $fileData;
+
+        // Clean all data
+        $this->cleanAllData();
+
+        // Get all manageable model columns and set to tableColumns
+        $manageableModel = (new $this->manageableModelClass)->getModelInstance();
+        $manageableModelColumns = Schema::getColumnListing($manageableModel->getTable());
+        $manageableModelColumns = array_diff($manageableModelColumns, ['id']);
+        $this->data['tableColumns'] = array_combine($manageableModelColumns, $manageableModelColumns);
+        $this->data['tableColumns'] = ['__wrla_unset_column__' => '❌ Select a column'] + $this->data['tableColumns'];
+
+        // Do best we can at mapping keys to columns
+        $this->autoMapHeadersToColumns();
+
+        $this->debugInfo = $this->data;
     }
 
     public function mount(string $manageableModelClass)
@@ -68,5 +111,59 @@ class ImportDataModal extends ModalComponent
         return view(WRLAHelper::getViewPath('livewire.import-data-modal'), [
 
         ]);
+    }
+
+    /**
+     * Cleans all data (Headers and Rows) from the currently set data
+     * 
+     * @return void
+     */
+    public function cleanAllData()
+    {
+        
+        // Clean headers
+        foreach ($this->data['headers'] as $key => $value) {
+            // Trim
+            $this->data['headers'][$key] = trim($value);
+
+            // Remove \u{FEFF} (UTF-8 BOM) from the start of the string
+            $this->data['headers'][$key] = preg_replace('/^\x{FEFF}/u', '', $this->data['headers'][$key]);
+
+            // Remove all special characters but allow spaces
+            $this->data['headers'][$key] = preg_replace('/[^A-Za-z0-9 ]/', '', $this->data['headers'][$key]);
+        }
+
+        // Clean rows
+        foreach ($this->data['rows'] as $rowKey => $row) {
+            foreach ($row as $key => $value) {
+                // Trim
+                $this->data['rows'][$rowKey][$key] = trim($value);
+
+                // Remove all special characters but allow spaces
+                $this->data['rows'][$rowKey][$key] = preg_replace('/[^A-Za-z0-9 ]/', '', $this->data['rows'][$rowKey][$key]);
+            }
+        }
+    }
+
+    public function autoMapHeadersToColumns()
+    {
+        // Loop through all map keys to columns
+        foreach ($this->data['headers'] as $headerIndex => $header) {
+            $this->data['headersMappedToColumns']["index_$headerIndex"] = null;
+
+            // Loop through all table columns
+            foreach ($this->data['tableColumns'] as $actualColumn) {
+                $header = str($header)->lower()->replace(' ', '_')->__toString();
+
+                // If the example column is similar to the table column
+                if (str($actualColumn) == $header) {
+                    // Set the map key to column
+                    $this->data['headersMappedToColumns']["index_$headerIndex"] = $actualColumn;
+                }
+            }
+        }
+
+        // Force re-render
+        $this->render();
     }
 }
--- src/resources/views/themes/default/livewire/import-data-modal.blade.php
+++ src/resources/views/themes/default/livewire/import-data-modal.blade.php
@@ -7,17 +7,57 @@
     <hr class="border border-gray-300 mt-[6px] mb-3">
 
     {{-- Form --}}
-    {!! view($WRLAHelper::getViewPath('components.forms.input-file'), [
-        'label' => '1. Import .csv file',
-        'options' => [
-            'notes' => 'Please upload a .csv file to import data into the system.',
-        ],
-        'attributes' => new \Illuminate\View\ComponentAttributeBag([
-            'name' => 'file',
-            'wire:model.live' => 'file',
-            'class' => ''
-        ])
-    ])->render() !!}
+    <div class="flex flex-col gap-4">
+        <div>
+            {!! view($WRLAHelper::getViewPath('components.forms.input-file'), [
+                'label' => 'Import .csv file',
+                'options' => [
+                    'notes' => 'NOTE: The first row of the file MUST be a list of headers.',
+                ],
+                'attributes' => new \Illuminate\View\ComponentAttributeBag([
+                    'name' => 'file',
+                    'wire:model.live' => 'file',
+                    'class' => ''
+                ])
+            ])->render() !!}
+        </div>
 
-    @dump($file)
+        {{-- If associate with columns data is set, we allow the user to associate the columns with table --}}
+        @if(count($data['headersMappedToColumns']) > 0)
+            <div class="grid grid-cols-4 gap-4">
+                @foreach($data['headers'] as $headerIndex => $header)
+                    {!! view($WRLAHelper::getViewPath('components.forms.input-select'), [
+                        'label' => $header,
+                        'items' => $data['tableColumns'],
+                        'options' => [
+                            
+                        ],
+                        'attributes' => new \Illuminate\View\ComponentAttributeBag([
+                            'wire:key' => "mapped-header-$headerIndex",
+                            'wire:model.live' => "data.headersMappedToColumns.index_$headerIndex",
+                        ])
+                    ])->render() !!}
+                @endforeach
+            </div>
+        @endif
+    </div>
+
+    <br />
+
+    <p class="py-2 font-medium">Debug: Headers mapped to columns</p>
+    @foreach($debugInfo['headersMappedToColumns'] ?? [] as $key => $value)
+        @dump([$key => $value])
+    @endforeach
+
+    <p class="py-2 font-medium">Debug: Headers</p>
+    @foreach($debugInfo['headers'] ?? [] as $key => $value)
+        @dump([$key => $value])
+    @endforeach
+
+    <br />
+    <p class="py-2 font-medium">Debug: First 3 rows</p>
+    @foreach($debugInfo['rows'] ?? [] as $key => $value)
+        @break($loop->index == 3)
+        @dump($value)
+    @endforeach
 </x-wrla-modal-layout>
