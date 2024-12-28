77222c314bfa633c7f31e99893dcbf8b3b10cb9e
Craig Dennis <craig@deliverysoftware.co.uk>
2024-12-28 09:54:25 +0000

    Add CSRF token meta tag to auth layout for security

--- src/resources/views/themes/default/layouts/auth-layout.blade.php
+++ src/resources/views/themes/default/layouts/auth-layout.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    {{-- <title>@yield('title', '(page title not set)') - WebRegulate Admin</title> --}}
+    <meta name="csrf-token" content="{{ csrf_token() }}"/>
     <title>{{ $WRLAHelper::buildPageTitle(
         app()->view->getSections()['title'] ?? '(page title not set)',
     ) }}</title>
