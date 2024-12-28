f7c1a7ff0d90809e66cc45a42cd513c160c0b642
Craig Dennis <craig@deliverysoftware.co.uk>
2024-12-28 09:45:02 +0000

    Update CSRF token meta tag in admin layout for consistency

--- src/resources/views/themes/default/layouts/admin-layout.blade.php
+++ src/resources/views/themes/default/layouts/admin-layout.blade.php
@@ -10,7 +10,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta name="csrf_token" value="{{ csrf_token() }}"/>
+    <meta name="csrf-token" content="{{ csrf_token() }}"/>
     <title>{{ $WRLAHelper::buildPageTitle(
         app()->view->getSections()['title'] ?? '(page title not set)',
     ) }}</title>
