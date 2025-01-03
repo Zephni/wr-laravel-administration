a7a684affff49c1fcab3e9e0390ff1085b39730f
Craig Dennis <craig@deliverysoftware.co.uk>
2025-01-03 18:55:18 +0000

    Update input-file component to improve loading state handling with specific wire targets

--- src/resources/views/components/forms/input-file.blade.php
+++ src/resources/views/components/forms/input-file.blade.php
@@ -35,11 +35,11 @@
         <div type="button" class="px-3 py-1.5 text-white bg-primary-600 hover:bg-primary-500 font-medium rounded-lg group-hover:bg-primary-500">
             Browse
         </div>
-        <div wire:loading class="ml-2 flex gap-3">
+        <div wire:loading wire:target="livewireData.{{ $attributes->get('name') }}" class="ml-2 flex gap-3">
             <i class="fas fa-spinner fa-spin"></i>
             <span>Uploading file, please wait this can take some time...</span>
         </div>
-        <span wire:loading.remove id="{{ $id }}-filename" class="ml-2" x-text="displayText"></span>
+        <span wire:loading.remove wire:target="livewireData.{{ $attributes->get('name') }}" id="{{ $id }}-filename" class="ml-2" x-text="displayText"></span>
     </label>
 </div>
 
