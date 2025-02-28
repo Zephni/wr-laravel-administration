186ef3886598640acfe4ae3c64ebb9fa6af9b021
Craig Dennis <craig@deliverysoftware.co.uk>
2025-02-28 11:07:53 +0000

    Update z-index values for sticky table headers in browse view

--- src/resources/views/themes/default/livewire/manageable-models/browse.blade.php
+++ src/resources/views/themes/default/livewire/manageable-models/browse.blade.php
@@ -103,7 +103,7 @@
                             </div>
                         </th>
                     @endforeach
-                    <th class="px-3 py-2 bg-slate-700 dark:bg-slate-700 border-b border-slate-400 dark:border-slate-600 sticky right-0 z-10"></th>
+                    <th class="sticky right-0 z-[4] px-3 py-2 bg-slate-700 dark:bg-slate-700 border-b border-slate-400 dark:border-slate-600"></th>
                 </tr>
             </thead>
             <tbody>
@@ -132,7 +132,7 @@
                                 </div>
                             </td>
                         @endforeach
-                        <td class="px-3 py-2 sticky right-0 z-10 @if($k % 2 == 0) bg-slate-100 dark:bg-slate-800 @else bg-slate-200 dark:bg-slate-700 @endif">
+                        <td class="px-3 py-2 sticky right-0 z-[4] @if($k % 2 == 0) bg-slate-100 dark:bg-slate-800 @else bg-slate-200 dark:bg-slate-700 @endif">
                             <div class="flex justify-end gap-2 text-sm">
                                 @foreach ($manageableModel->getInstanceActionsFinal() as $browseAction)
                                     {!! $browseAction->render() !!}
