be892dce006a8eb52174d77ec975304c97ecda8a
Craig Dennis <craig@deliverysoftware.co.uk>
2025-05-01 08:52:52 +0100

    Add log-viewer asset publishing in InstallCommand: enhance installation process by including log-viewer assets

--- src/Commands/InstallCommand.php
+++ src/Commands/InstallCommand.php
@@ -42,6 +42,9 @@ class InstallCommand extends Command
             '--tag' => 'wrla-assets',
         ]);
 
+        // Publish log-viewer assets
+        $this->call('log-viewer:publish');
+
         // Create UserData.php model in app/Models
         $envConnection = env('DB_CONNECTION', 'mysql');
         WRLAHelper::generateFileFromStub(
