7f78c7978aaa64a92be5ea0358e7de347fb375d9
Craig Dennis <craig@deliverysoftware.co.uk>
2025-05-09 12:01:10 +0100

    Refactor InstallCommand: streamline log-viewer asset publishing and improve code readability

--- src/Commands/InstallCommand.php
+++ src/Commands/InstallCommand.php
@@ -36,6 +36,7 @@ class InstallCommand extends Command
             '--tag' => 'wrla-config',
         ]);
 
+
         // Publish assets
         $this->call('vendor:publish', [
             '--provider' => \WebRegulate\LaravelAdministration\WRLAServiceProvider::class,
@@ -44,9 +45,7 @@ class InstallCommand extends Command
 
         // Publish log-viewer assets
         $this->call('log-viewer:publish');
-        $this->call('log-viewer:publish', [
-            '--tag' => 'log-viewer-config',
-        ]);
+        $this->call('vendor:publish --tag="log-viewer-config"');
 
         // Create UserData.php model in app/Models
         $envConnection = env('DB_CONNECTION', 'mysql');
