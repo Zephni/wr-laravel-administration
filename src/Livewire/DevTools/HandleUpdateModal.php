58b402e6940f05d290de5d577700aeb899c9d876
Craig Dennis <craig@deliverysoftware.co.uk>
2025-06-18 11:20:07 +0100

    Refactor HandleUpdateModal to simplify command execution and update console output display

--- src/Livewire/DevTools/HandleUpdateModal.php
+++ src/Livewire/DevTools/HandleUpdateModal.php
@@ -14,8 +14,6 @@ class HandleUpdateModal extends ModalComponent
      */
     public string $consoleOutput = '';
 
-    public string $command = '';
-
     public function mount()
     {
         // 404 if dev tools are not enabled for this user
@@ -34,29 +32,6 @@ class HandleUpdateModal extends ModalComponent
 
     public function runCommand()
     {
-        $command = $this->command;
-        $this->command = '';
-
-        // 1. Create a temporary "stream" in memory
-        $inputStream = fopen('php://memory', 'r+');
-
-        // 2. Write a newline character to the stream (this is "Enter")
-        fwrite($inputStream, "\n");
-
-        // 3. Rewind the stream so the process can read it from the start
-        rewind($inputStream);
-
-        // 4. Use run() and feed the stream resource directly to input()
-        $result = Process::path(base_path())
-            ->env($_SERVER)
-            ->input($inputStream) // This is the corrected line
-            ->run($command);
-
-        // The command will now run to completion
-        $this->consoleOutput = $this->consoleOutput.'
-'.$result->output();
-
-        // 5. Close the stream resource
-        fclose($inputStream);
+        dd('Working on this...');
     }
 }
--- src/resources/views/themes/default/livewire/dev-tools/handle-update-modal.blade.php
+++ src/resources/views/themes/default/livewire/dev-tools/handle-update-modal.blade.php
@@ -8,16 +8,12 @@
     </div>
 
     <div class="bg-slate-900 text-slate-200 p-4 rounded-lg mt-4">
-        <h3 class="text-lg font-semibold mb-2">Command Output:</h3>
+        <h3 class="text-lg font-semibold mb-2">Console Output:</h3>
         <div class="w-full max-h-96 overflow-auto">
             <pre class="whitespace-pre-wrap">{{ $consoleOutput }}</pre>
         </div>
         <div class="flex gap-4 mt-4">
-            <input wire:model="command" 
-                wire:keydown.enter="runCommand"
-                class="w-full px-2 py-1 border-t border-gray-300 bg-slate-900 text-white focus:!ring-0"
-                placeholder="Input" />
-            {{-- <button wire:click="runCommand" class="whitespace-nowrap">✅ Run command</button> --}}
+            <button wire:click="runCommand" class="whitespace-nowrap">✅ Check updates</button>
         </div>
     </div>
 
