20a60f2f7a8c42cfb07188cb988de603d6dbab6c
Craig Dennis <craig@deliverysoftware.co.uk>
2025-06-26 11:40:30 +0100

    Enhance EditUserCommand to improve user editing experience; add password field handling and special commands for user data relationships

--- src/Commands/EditUserCommand.php
+++ src/Commands/EditUserCommand.php
@@ -100,40 +100,141 @@ class EditUserCommand extends Command
         // Display all fields for this user
         $this->info('User found:');
 
-        // If less or equal to 9 columns, display as table
-        if(count($userColumns) <= 9) {
-            $this->table(
-                $userColumns,
-                [$userValues]
-            );
-        }
-        // Otherwise display as a list
-        else {
-            foreach ($userValues as $key => $value) {
-                $this->line("<fg=yellow>{$key}:</> {$value}");
-            }
-        }
-
         while (true) {
+            // Display user fields and values
+            $this->displayKeysAndValues($userValues);
+
+            // Get password field from user data model
+            try {
+                $passwordField = WRLAHelper::getUserDataModelClass()::getPasswordField();
+            } catch (\Exception $e) {
+                $this->error('Error getting wrlaUserData password field: ' . $e->getMessage());
+                dd('Aborting due to error.');
+            }
+
             // Special cases
             $specialCases = [
-                'password' => [
+                $passwordField => [
                     'message' => 'Enter new password, min 6 characters (will be hashed)',
                     'validation' => 'required|string|min:6',
-                    'function' => fn ($value) => Hash::make($value),
+                    'function' => function ($value) {
+                        try {
+                            return WRLAHelper::getUserDataModelClass()::hashPassword($value); 
+                        } catch (\Exception $e) {
+                            $this->error('Error hashing password: ' . $e->getMessage());
+                            dd('Aborting due to hashing error.');
+                        }
+                    },
                 ],
             ];
 
+            // Special one off options
+            $this->line(PHP_EOL . 'Special commands:');
+            $specialUserOptions = ['wrla:user-data', 'exit'];
+            foreach ($specialUserOptions as $option) {
+                $this->line("<fg=yellow>{$option}</>: Edit user data relationship");
+            }
+
             // Ask for the field to edit
-            $field = $this->ask('Which field do you want to edit?');
+            $field = $this->ask("Which field do you want to edit, or type a special command from above");
+
+            // If user wants to exit
+            if (strtolower($field) === 'exit') {
+                $this->info('Exiting user edit command.');
+                return 0;
+            }
+
+            // If wrla_user_data is entered, handle relationship editing
+            if ($field === 'wrla:user-data') {
+                // Get wrla_user_data database information
+                $wrlaUserDataInstance = app(WRLAHelper::getUserDataModelClass());
+                $wrlaUserDataTable = str($wrlaUserDataInstance->getTable())->afterLast('.')->toString();
+                $wrlaUserDataConnection = $wrlaUserDataInstance->getConnectionName();
+                $wrlaUserDataColumns = WRLAHelper::getTableColumns($wrlaUserDataTable, $wrlaUserDataConnection);
+
+                while(true) {
+                    // Display wrla_user_data fields as table
+                    $this->info('Available fields for wrla_user_data:');
+                    $this->displayKeysAndValues($user->wrlaUserData->toArray());
+    
+                    // Ask for the field to edit
+                    $specialWrlaUserDataCases = [
+                        'make:admin' => [
+                            'message' => 'Give admin permission',
+                            'function' =>  fn($user) => $user->wrlaUserData->setPermission('admin', true)
+                        ],
+                        'make:master' => [
+                            'message' => 'Give master permission',
+                            'function' =>  fn($user) => $user->wrlaUserData->setPermission('master', true) && $user->wrlaUserData->setPermission('admin', true)
+                        ],
+                        'revoke:admin' => [
+                            'message' => 'Revoke admin permission',
+                            'function' =>  fn($user) => $user->wrlaUserData->setPermission('admin', false)
+                        ],
+                        'revoke:master' => [
+                            'message' => 'Revoke master and admin permissions',
+                            'function' =>  fn($user) => $user->wrlaUserData->setPermission('master', false) && $user->wrlaUserData->setPermission('admin', false)
+                        ],
+                        'revoke:all' => [
+                            'message' => 'Revoke all permissions',
+                            'function' =>  function($user) {
+                                $wrlaUserData = $user->wrlaUserData;
+                                $wrlaUserData->permissions = '[]';
+                                $wrlaUserData->save();
+                            }
+                        ],
+                        'back' => [
+                            'message' => 'Go back to user edit',
+                            'function' => fn($user) => 'back'
+                        ],
+                    ];
+
+                    // Show special commands
+                    $this->info(PHP_EOL.'Special commands:');
+                    foreach ($specialWrlaUserDataCases as $key => $specialCase) {
+                        $this->line("<fg=yellow>{$key}:</> {$specialCase['message']}");
+                    }
+
+                    $this->line('');
+                    $field = $this->ask('Which user data field do you want to edit, or use special commands above');
+
+                    // Check if the field is a special case
+                    if (array_key_exists($field, $specialWrlaUserDataCases)) {
+                        $return = $specialWrlaUserDataCases[$field]['function']($user);
+                        $this->line('');
+
+                        if($return === 'back') {
+                            $this->info('Going back to user edit.');
+                            continue 2; // Go back to the main user edit loop
+                        }
+
+                        continue;
+                    }
+                    
+                    // Check if the field exists in wrla_user_data
+                    if (!in_array($field, $wrlaUserDataColumns)) {
+                        $this->error("Field {$field} does not exist in {$wrlaUserDataTable}. Please try again.");
+                        continue;
+                    }
+
+                    $this->info("Field {$wrlaUserDataTable}.{$field} updated successfully!");
+                    break;
+                }
+            }
 
             // If special case, ask with custom message
             if (array_key_exists($field, $specialCases)) {
                 $message = $specialCases[$field]['message'];
                 $validation = $specialCases[$field]['validation'];
                 $function = $specialCases[$field]['function'];
-                // Default case
-            } else {
+            }
+            // If field does not exist in user model, show error
+            elseif (!in_array($field, $userColumns)) {
+                $this->error("Field {$field} does not exist in {$userTable}. Please try again.");
+                continue;
+            }
+            // Default case
+            else {
                 $message = "What is the new value for the {$field} field?";
                 $validation = 'required|string';
                 $function = null;
@@ -163,10 +264,7 @@ class EditUserCommand extends Command
             $user->$field = $newValue;
             $user->save();
             $this->info('User updated successfully! New user data:');
-            $this->table(
-                array_keys($user->toArray()),
-                [$user->toArray()]
-            );
+            $this->displayKeysAndValues($user->only($userColumns));
 
             // Ask if the user wants to edit another field
             $anotherField = $this->confirm('Do you want to edit another field?', true);
@@ -176,5 +274,23 @@ class EditUserCommand extends Command
         }
 
         return 0;
+    }
+
+    private function displayKeysAndValues(array $keyValues)
+    {
+        // If less or equal to 9 columns, display as table
+        if(count($keyValues) <= 7) {
+            $this->table(
+                array_keys($keyValues),
+                [$keyValues],
+                'compact'
+            );
+        }
+        // Otherwise display as a list
+        else {
+            foreach ($keyValues as $key => $value) {
+                $this->line("<fg=yellow>{$key}:</> {$value}");
+            }
+        }
     }
 }
