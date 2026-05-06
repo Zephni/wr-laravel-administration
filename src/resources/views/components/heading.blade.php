ff0f283ae417611a7e9aa51fbd4777e5ab1aff62
Craig Dennis <craig@deliverysoftware.co.uk>
2026-05-06 10:16:02 +0100

    Add heading component with title and icon props

--- src/resources/views/components/heading.blade.php
+++ src/resources/views/components/heading.blade.php
@@ -8,5 +8,5 @@
         <i class="{{ $icon }} text-slate-700 dark:text-white mr-1"></i>
         {{ $title }}
     </h1>
-    <hr class="border-b border-slate-400 w-80 mt-1.5 mb-3">
+    <hr class="border-b border-slate-400 w-80 mt-1 mb-5">
 </div>
